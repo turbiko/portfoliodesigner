02d6f0738d602f805a450fde95bfa9733588fd41
Andriy Voznyuk <andreyv@ukr.net>
2022-08-23 21:29:15 +0200

    Update dev.py

--- core/settings/dev.py
+++ core/settings/dev.py
@@ -8,7 +8,7 @@ SECRET_KEY = "django-insecure-*&x)0kf4c0ed%18_@bbu0s+v5akech-2q*ro&z^lt5@wucm1x!
 
 # SECURITY WARNING: define the correct hosts in production!
 ALLOWED_HOSTS = ["127.0.0.1", "design.argentum.ua", "10.1.100.200", "localhost" ]
-CSRF_TRUSTED_ORIGINS = ['design.argentum.ua']
+CSRF_TRUSTED_ORIGINS = ['https://design.argentum.ua']
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
