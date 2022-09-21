04c0fbedea4216fcae11c2c027168862668e1d3e
Andriy Voznyuk <andreyv@ukr.net>
2022-09-21 16:25:10 +0200

    cron update

--- core/tools.py
+++ core/tools.py
@@ -6,4 +6,4 @@ from datetime import datetime
 def file_path(instance, filename):
     _, file_extension= os.path.splitext(filename)
     filename= datetime.now().strftime('%Y/%m/%d/')+'file-'+uuid.uuid4().hex
-    return '{filename}{ext}'.format( filename= filename, ext= file_extension)
+    return '{filename}{ext}'.format(filename=filename, ext=file_extension)
--- project/models.py
+++ project/models.py
@@ -1,21 +1,45 @@
 from django.db import models
 from django.utils.translation import activate, gettext_lazy as _
+from modelcluster.contrib.taggit import ClusterTaggableManager
 from modelcluster.fields import ParentalKey
+from taggit.models import TaggedItemBase
 
 from wagtail.models import Page, Orderable
 from wagtail.fields import RichTextField
 from wagtail.admin.panels import FieldPanel, InlinePanel
 from wagtail.search import index
+from core import tools
+
+class ProjectPageTag(TaggedItemBase):
+    content_object = ParentalKey(
+        'ProjectPage',
+        related_name='tagged_items',
+        on_delete=models.CASCADE
+    )
+
 
 class ProjectPage(Page):
     date = models.DateField( blank=True)
     about_project = RichTextField(blank=True)
     client = models.CharField( max_length=250, blank=True)
-    tags = models.CharField(max_length=100, blank=True)
+    # tags = models.CharField(max_length=100, blank=True)
+    tags = ClusterTaggableManager(through=ProjectPageTag, blank=True)
     categories = models.CharField(max_length=100, blank=True)
-    pic1 = models.FileField(upload_to='pictures/', blank=True)
-    pic2 = models.FileField(upload_to='pictures/', blank=True)
-    pic3 = models.FileField(upload_to='pictures/', blank=True)
+    project_teaser = models.ForeignKey(
+        'wagtailimages.Image', on_delete=models.SET_NULL,  related_name='+', null=True
+    )
+    file = models.FileField(
+        upload_to=tools.file_path, blank=True)  # TODO: upload_to method need to know project and folder name for create dirs
+
+    content_panels = Page.content_panels + [
+        FieldPanel('date'),
+        FieldPanel('about_project'),
+        FieldPanel('client'),
+        FieldPanel("tags"),
+        FieldPanel('categories'),
+        FieldPanel('project_teaser'),
+        InlinePanel('gallery_images', label="Portfolio images"),
+    ]
 
     class Meta:
         verbose_name = "Project Page"
@@ -30,27 +54,17 @@ class ProjectPage(Page):
     def get_project(self):
         return self
 
-    content_panels = Page.content_panels + [
-        FieldPanel('date'),
-        FieldPanel('about_project'),
-        FieldPanel('client'),
-        FieldPanel('tags'),
-        FieldPanel('categories'),
-        FieldPanel('pic1'),
-        FieldPanel('pic2'),
-        FieldPanel('pic3'),
-        InlinePanel('gallery_images', label="gallery images"),
-    ]
+
 
 
 class ProjectPageGalleryImage(Orderable):
-    page = ParentalKey(ProjectPage, on_delete=models.CASCADE, related_name='gallery_images')
-    image = models.ForeignKey(
-        'wagtailimages.Image', on_delete=models.CASCADE, related_name='+'
-    )
-    caption = models.CharField(blank=True, max_length=250)
+    page = ParentalKey(ProjectPage, on_delete=models.CASCADE, related_name='gallery_images', null=True, default=None)
+    file = models.FileField(
+            upload_to=tools.file_path, blank=True)  # TODO: upload_to method need to know project and folder name for create dirs
+    name = models.CharField(blank=True, max_length=250)
 
     panels = [
-        FieldPanel('image'),
-        FieldPanel('caption'),
+        FieldPanel('name'),
+        FieldPanel('file'),
     ]
+
